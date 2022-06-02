43c9eac3f358d3f569bc1ed6eb73f34665dd6459
naceur-med <naceurpro.bm@gmail.com>
2022-06-02 21:07:19 +0200

    Enter

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+    $bdd = new PDO('mysql:host=localhost;dbname=cyberphiles;charset=utf8', 'root', '');
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
 }
