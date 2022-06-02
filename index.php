35e913f72b705e6b084a85c6fd4aa2588c02e3c3
naceur-med <naceurpro.bm@gmail.com>
2022-06-02 21:04:18 +0200

    Enter

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<?php
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+if (isset($_POST['envoyer le message'])) {
+    $nom = $_POST['nom'];
+    $email = $_POST['email'];
+    $numero = $_POST['numero'];
+    $message = $_POST['message'];
+
+    $sql = ("INSERT INTO client(nom, email, numero, message) VALUES (:nom, :email, :numero, :message)");
+    $stmt = $conn->prepare($sql);
+
+    $stmt->bindParam(':nom', $nom);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':numero', $numero);
+    $stmt->bindParam(':message', $message);
+    $stmt->execute();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,10 +41,6 @@
 </head>
 
 <body id="page-top">
-
-<?php 
-          include("include.php"); 
-        ?>
 
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
         <div class="container">
